a47a5cf07317a2cb20b8b9268b067d3c4ae0f9af
Siddhant Sarkar <siddhant.gecbilaspur@gmail.com>
2020-04-07 02:46:36 +0530

    Update run.py

--- app/run.py
+++ app/run.py
@@ -2,9 +2,8 @@ import json
 import plotly
 import pandas as pd
 import nltk
-nltk.download(['punkt', 'wordnet','averaged_perceptron_tagger'])
 from nltk.stem import WordNetLemmatizer
-from nltk.tokenize import word_tokenize
+from nltk.tokenize import word_tokenize,sent_tokenize
 from sklearn.base import BaseEstimator, TransformerMixin
 from flask import Flask
 from flask import render_template, request, jsonify
@@ -15,34 +14,6 @@ from sqlalchemy import create_engine
 
 app = Flask(__name__)
 
-class StartingVerbExtractor(BaseEstimator, TransformerMixin):
-
-    def starting_verb(self, text):
-        sentence_list = nltk.sent_tokenize(text)
-        for sentence in sentence_list:
-            pos_tags = nltk.pos_tag(tokenize(sentence))
-            first_word, first_tag = pos_tags[0]
-            if first_tag in ['VB', 'VBP'] or first_word == 'RT':
-                return True
-        return False
-
-    def fit(self, X, y=None):
-        return self
-
-    def transform(self, X):
-        X_tagged = pd.Series(X).apply(self.starting_verb)
-        return pd.DataFrame(X_tagged)
-
-def tokenize(text):
-    tokens = word_tokenize(text)
-    lemmatizer = WordNetLemmatizer()
-
-    clean_tokens = []
-    for tok in tokens:
-        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
-        clean_tokens.append(clean_tok)
-
-    return clean_tokens
 
 # load data
 engine = create_engine('sqlite:///data/DisasterResponse.db')
@@ -133,4 +104,33 @@ def go():
 
 
 if __name__ == '__main__':
+    def tokenize(text):
+        tokens = word_tokenize(text)
+        lemmatizer = WordNetLemmatizer()
+
+        clean_tokens = []
+        for tok in tokens:
+            clean_tok = lemmatizer.lemmatize(tok).lower().strip()
+            clean_tokens.append(clean_tok)
+
+        return clean_tokens
+    
+    class StartingVerbExtractor(BaseEstimator, TransformerMixin):
+
+        def starting_verb(self, text):
+            sentence_list = nltk.sent_tokenize(text)
+            for sentence in sentence_list:
+                pos_tags = nltk.pos_tag(tokenize(sentence))
+                first_word, first_tag = pos_tags[0]
+                if first_tag in ['VB', 'VBP'] or first_word == 'RT':
+                    return True
+            return False
+
+        def fit(self, X, y=None):
+            return self
+
+        def transform(self, X):
+            X_tagged = pd.Series(X).apply(self.starting_verb)
+            return pd.DataFrame(X_tagged)
+
     app.run()
