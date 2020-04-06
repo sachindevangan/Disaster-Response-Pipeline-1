4e606f668064ea5505001e4de7e5b2dca3077299
Siddhant Sarkar <siddhant.gecbilaspur@gmail.com>
2020-04-06 16:44:21 +0530

    finished coding process_data.py

--- data/process_data.py
+++ data/process_data.py
@@ -1,16 +1,101 @@
 import sys
-
+import pandas as pd
+from sqlalchemy import create_engine
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+    """ 
+    Loading Data From Multiple Files. 
+  
+    Merging Them Together To Form A Single DataFrame And Returning It. 
+  
+    Parameters: 
+    messages_filepath (str): Filepath Where Messages Are Stored.
+  	categories_filepath (str): Filepath Where Categories Are Stored.
+    
+    Returns: 
+    merged_df (DataFrame): Combined DataFrame Object
+  
+    """
 
+    # reading csv files using pandas
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+
+    # merging above dataframes 
+    merged_df = pd.merge(messages,categories)
+
+    return merged_df
 
 def clean_data(df):
-    pass
+    """ 
+    Data Cleaning Process. 
+  
+    Takes In A Pandas DataFrame And Seperates The Categories Into Individual
+    Category Columns And Then Merging Them Back To The DataFrame ,Finally 
+    Returning New DataFrame. 
+  
+    Parameters: 
+    df (DataFrame): Pandas DataFrame Object Containing The Data.
+  
+    Returns: 
+    cleaned_df (DataFrame): Pandas DataFrame Object Containing The Cleaned Data.
+  
+    """
+
+    # creating a dataframe of the 36 individual category columns
+    categories = df['categories'].str.split(';',expand = True)
+
+    # selecting the first row of the categories dataframe to extract column names
+    row = categories.loc[0]
+
+    # creating a list of category column names
+    category_colnames = [category[:len(category)-2] for category in row ]
+
+    # renaming the columns of `categories` dataframe
+    categories.columns = category_colnames
+
+    # now converting category values to just numbers 0 or 1
+    for column in categories:
+
+    	# setting each value to be the last character of the string
+    	categories[column] = categories[column].str[-1]
+    
+    	# converting column from string to numeric
+    	categories[column] = pd.to_numeric(categories[column])
+
+    # replacing categories column in df with new category columns.
+    df = df.drop('categories',axis = 1)
+
+    # concatenating the original dataframe with the new `categories` dataframe
+    df = pd.concat([df,categories],axis = 1)
+
+    # removing duplicates
+    cleaned_df = df.drop_duplicates(keep = 'first')
+    
+    return cleaned_df
 
 
 def save_data(df, database_filename):
-    pass  
+    """ 
+    Save To Database Method. 
+  
+    Saving The Cleaned Pandas DataFrame Into A SQLite Database. 
+  
+    Parameters: 
+    df (DataFrame): Pandas DataFrame Object Containing The Cleaned Data.
+    database_filename (str) : Filename Of Database.
+  
+    """
+
+    # formatting database name properly
+    db_name = 'sqlite:///{}'.format(database_filename)
+
+    # initiating SQLAlchemy Engine
+    engine = create_engine(db_name)
+
+    # using pandas to save the DataFrame to the database
+    df.to_sql('DisasterResponse', engine, index=False)
+ 
 
 
 def main():
