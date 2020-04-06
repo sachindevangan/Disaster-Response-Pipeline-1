48a7daaee9b9052a45c8fd3078b3ae7fdab705d9
Siddhant Sarkar <siddhant.gecbilaspur@gmail.com>
2020-04-07 02:27:14 +0530

    Update run.py

--- app/run.py
+++ app/run.py
@@ -45,7 +45,7 @@ def tokenize(text):
     return clean_tokens
 
 # load data
-engine = create_engine('sqlite:///../data/DisasterResponse.db')
+engine = create_engine('sqlite:///data/DisasterResponse.db')
 df = pd.read_sql_table('DisasterResponse', engine)
 
 # load model
