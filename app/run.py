5da8388b6fcd0540c6f20f68163b1d057b3e013a
Siddhant Sarkar <siddhant.gecbilaspur@gmail.com>
2020-04-07 14:27:01 +0530

    Update run.py

--- app/run.py
+++ app/run.py
@@ -17,11 +17,11 @@ app = Flask(__name__)
 
 
 # load data
-engine = create_engine('sqlite:///../data/DisasterResponse.db')
+engine = create_engine('sqlite:///data/DisasterResponse.db')
 df = pd.read_sql_table('DisasterResponse', engine)
 
 # load model
-model = joblib.load("../models/classifier.pkl")
+model = joblib.load("models/classifier.pkl")
 
 
 # index webpage displays cool visuals and receives user input text for model
