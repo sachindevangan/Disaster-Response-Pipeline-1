171eca437c57f875966ae14414f7f4d0cf40292f
Siddhant Sarkar <siddhant.gecbilaspur@gmail.com>
2020-04-09 03:45:56 +0530

    Update run.py

--- app/run.py
+++ app/run.py
@@ -14,7 +14,7 @@ from flask import render_template, request, jsonify
 from plotly.graph_objs import Bar
 from sklearn.externals import joblib
 from sqlalchemy import create_engine
-from app.tokenizer_function import Tokenizer,StartingVerbExtractor
+from herokutokenizer import Tokenizer,StartingVerbExtractor
 
 app = Flask(__name__)
 
